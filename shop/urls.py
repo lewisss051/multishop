455c0c874fb3fc096f9f1408001db7dc0cfb9cf6
Jamol <jamol.nabiev@gmail.com>
2025-03-14 20:57:27 +0500

    detail tugadi

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path("",Index.as_view(),name="index"),
-    path("products/<int:pk>/",ProductDetail.as_view(),name="detail")
+    path("products/<int:pk>/",ProductDetail.as_view(),name="detail"),
+    path("index",Index.as_view(),name="multishop")
 ]
